7e632d18c69bcc2a9203e2f557ef7ab2191b2d7c
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-04-08 10:47:16 +0700

    Update 1.0

---
+++ aboutme.php
@@ -0,0 +1,9 @@
+<?php
+
+// Let the code begin
+echo "Nama : Muhammad Farras Jibran";
+echo "Kelas : XI RPL 1";
+echo "Asal Sekolah : SMK TARUNA BHAKTI";
+echo "Alamat : Perum Griya Kalibaru Blok B2NO4";
+
+?>
